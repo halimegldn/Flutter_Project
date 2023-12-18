c7a14ee01782eafbc1105dd0c6931b4461c1ff7b
halimegldn <87153723+halimegldn@users.noreply.github.com>
2023-12-18 10:41:01 +0300

    Update Login_Page.dart

--- Login_Page.dart
+++ Login_Page.dart
@@ -1,114 +1,111 @@
 import 'package:flutter/material.dart';
 import 'Page1.dart';
+import 'auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatelessWidget {
-  var formKey = GlobalKey<FormState>();
-  // "mail","sifre", "kullanıcı adı" parametreleri değişmez olarak tanımlandı
-  final String kullaniciAdi;
-  final String mail;
-  final String sifre;
+class LoginPage extends StatefulWidget {
+  //Login sayfası
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-  LoginPage({
-    required this.kullaniciAdi,
-    required this.mail,
-    required this.sifre,
-  });
+class _LoginPageState extends State<LoginPage> {
+  //Kullanıcı bilgilerini alma işlemi
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final AuthService _authService = AuthService();
+
+  Future<void> _login(BuildContext context) async {
+    // Kullanıcı giriş işlemi
+    User? user = await _authService.signIn(
+      _emailController.text,
+      _passwordController.text,
+    );
+
+    if (user != null) {
+      // Giriş başarılı, Page1 sayfasına yönlendir
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Page1()),
+      );
+    } else {
+      // Giriş başarısızsa hata mesajı gösterilebilir
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Hata'),
+            content: Text('Giriş başarısız. Lütfen tekrar deneyin.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Tamam'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Sayfanın arka planına resim eklendi
+      appBar: null,
       body: Container(
+        //Arkaplan rengi
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/arkaplan.jpg"),
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUoboEK2anBgaa0WO30_LdzwocTOOr1QcMlSFbhACsXw&s'),
             fit: BoxFit.cover,
           ),
+          color: Colors.white.withOpacity(0.3),
         ),
         child: Center(
-          // Sayfanın ortasına bir container eklendi
           child: Container(
-            //Container opaklığı ayarlandı
-            color: Colors.black.withOpacity(0.2),
-            padding: EdgeInsets.all(20.0),
-            //Container içine form eklendi
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'E-Mail',
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black, width: 1.3),
-                      ),
-                    ),
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    //Form Validation yapıldı
-                    validator: (tfgirdisi) {
-                      if (tfgirdisi?.isEmpty ?? true) {
-                        return 'E-Mail boş bırakılamaz';
-                      }
-                      return null;
-                    },
+            //Form oluşturma
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: _emailController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'E-posta',
+                    labelStyle: TextStyle(color: Colors.white),
                   ),
-                  SizedBox(height: 10.0),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Şifre',
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black, width: 1.3),
-                      ),
-                    ),
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    //Form Validation yapıldı
-                    validator: (tfgirdisi) {
-                      if (tfgirdisi?.isEmpty ?? true) {
-                        return 'Şifre boş bırakılamaz';
-                      }
-                      return null;
-                    },
+                ),
+                TextField(
+                  controller: _passwordController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Şifre',
+                    labelStyle: TextStyle(color: Colors.white),
                   ),
-                  SizedBox(height: 20.0),
-                  //Giriş Butonu oluşturuldu
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        vertical: 18.0,
-                        horizontal: 32.0,
-                      ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 16.0),
+                //Buton style özellikleri
+                ElevatedButton(
+                  onPressed: () => _login(context),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
                     ),
-                    child: Text('GİRİŞ'),
-                    //Butona tıklandığında yapılacak işlemler
-                    onPressed: () {
-                      if (formKey.currentState?.validate() ?? false) {
-                        // Yönlendirme işlemi:
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => MyApp(),
-                          ),
-                        );
-                      }
-                    },
+                    padding: EdgeInsets.symmetric(
+                      vertical: 18.0,
+                      horizontal: 32.0,
+                    ),
                   ),
-                ],
-              ),
+                  child:
+                      Text('Giriş Yap', style: TextStyle(color: Colors.white)),
+                ),
+              ],
             ),
           ),
         ),
@@ -116,8 +113,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
