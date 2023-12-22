45bde764247f3eadd11b556149a4a1c9fe3e802a
halimegldn <87153723+halimegldn@users.noreply.github.com>
2023-12-22 11:18:10 +0300

    Create auth_service.dart

---
+++ auth_service.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      print("Giriş hatası: ${e.message}");
+      return null;
+    }
+  }
+
+  Future<User?> register(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      print("Kullanıcı kaydı hatası: ${e.message}");
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
