5b73fe94e9f5b48bbbe908e0bc6aa33cdf657f05
halimegldn <87153723+halimegldn@users.noreply.github.com>
2023-12-22 11:19:53 +0300

    Update Register_Page.dart

--- Register_Page.dart
+++ Register_Page.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'login_page.dart';
+import 'login_page.dart'; // Eğer dosyanın adı farklıysa, doğru adı kullanın.
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //Firebase bağlantısı
+
   try {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    // print("Firebase bağlantısı başarılı!"); // Debug mesajı
+    // print("Firebase bağlantısı başarılı!"); // Debug mesajını kaldırdık
   } catch (e) {
     print("Firebase bağlantı hatası: $e");
   }
@@ -23,22 +23,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Debug banner'ı kaldırma işlemi
+      debugShowCheckedModeBanner: false, // Debug banner'ı kaldırdık
       home: RegisterPage(),
     );
   }
 }
 
-//Register sayfası
 class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  //Firebase sınıfından örnek oluşturma
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  //Kullanıcı bilgilerini alma işlemi
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
@@ -60,14 +57,13 @@ class _RegisterPageState extends State<RegisterPage> {
 
       print("Kullanıcı kaydı başarılı: ${userCredential.user!.uid}");
 
-      // Kayıt işlemi başarılı olduktan sonra oturum açma sayfası
+      // Kayıt işlemi başarılı olduktan sonra oturum açma sayfasını göster
       _showLoginPage();
     } on FirebaseAuthException catch (e) {
       print("Kullanıcı kaydı hatası: ${e.message}");
     }
   }
 
-  //Yönlendirme kodu
   void _showLoginPage() {
     Navigator.pushReplacement(
       context,
@@ -80,18 +76,16 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       appBar: null, // AppBar'ı kaldırdık
       body: Container(
-        //Sayfanın arkaplan rengi
         decoration: BoxDecoration(
           image: DecorationImage(
             image: NetworkImage(
                 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUoboEK2anBgaa0WO30_LdzwocTOOr1QcMlSFbhACsXw&s'),
             fit: BoxFit.cover,
           ),
-          color: Colors.black.withOpacity(0.3), // Opak beyaz arkaplan
+          color: Colors.white.withOpacity(0.3), // Opak beyaz arkaplan
         ),
         child: Center(
-          //Form oluşturma işlemi
-          child: Container(
+          child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -129,7 +123,6 @@ class _RegisterPageState extends State<RegisterPage> {
                   obscureText: true,
                 ),
                 SizedBox(height: 16.0),
-                //Buton style özellikleri
                 ElevatedButton(
                   onPressed: _register,
                   style: ElevatedButton.styleFrom(
