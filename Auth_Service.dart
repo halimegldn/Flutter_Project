1ceb34daebc5296889e0703798d43ce13fe81469
halimegldn <87153723+halimegldn@users.noreply.github.com>
2023-12-25 10:35:32 +0300

    Update Auth_Service.dart

--- Auth_Service.dart
+++ Auth_Service.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // Kullanıcı giriş işlemi
   Future<User?> signIn(String email, String password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
@@ -11,11 +12,13 @@ class AuthService {
       );
       return userCredential.user;
     } on FirebaseAuthException catch (e) {
+      // Giriş hatası durumunda hata mesajını yazdır
       print("Giriş hatası: ${e.message}");
       return null;
     }
   }
 
+  // Kullanıcı kaydı işlemi
   Future<User?> register(String email, String password) async {
     try {
       UserCredential userCredential =
@@ -25,11 +28,13 @@ class AuthService {
       );
       return userCredential.user;
     } on FirebaseAuthException catch (e) {
+      // Kullanıcı kaydı hatası durumunda hata mesajını yazdır
       print("Kullanıcı kaydı hatası: ${e.message}");
       return null;
     }
   }
 
+  // Kullanıcı çıkış işlemi
   Future<void> signOut() async {
     await _auth.signOut();
   }
